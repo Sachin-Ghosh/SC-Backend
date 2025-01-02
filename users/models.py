33641e582cd723ee067f95b6a4acdd857abe24b7
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-01-02 15:03:23 +0530

    Made changes in users

--- users/models.py
+++ users/models.py
@@ -64,6 +64,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15,null=True, blank=True)
     roll_number = models.CharField(max_length=20, null=True, blank=True)
     year_of_study = models.TextField(null=True, blank=True, choices=YEAR_TYPES)
+    reset_password_token = models.CharField(max_length=100, null=True, blank=True)
+    reset_password_token_valid_until = models.DateTimeField(null=True, blank=True)
     
     def __str__(self):
         return self.username
