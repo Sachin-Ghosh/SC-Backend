3ce9b9f001887f0b1364fd9388d42ef7f5dc8302
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-01-13 13:26:26 +0530

    Deployemnt to aws and rds connection 8

--- manage_prod.py
+++ manage_prod.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sc_backend.setting_prod')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sc_backend.settings_prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
